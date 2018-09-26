2dcc858fd633133e45ba23a5c00c522f9b896253
Nickolay Ilyushin <nickolay02@inbox.ru>
2018-09-26 10:27:59 +0300

    Now all arguments are passed by reference

--- particletk/create.hpp
+++ particletk/create.hpp
@@ -3,12 +3,12 @@
 namespace PTK {
 
   template<typename T, template<typename...> typename Ptr, typename... Args>
-  Ptr<T> create_ptr(Args... args) {
+  Ptr<T> create_ptr(Args const&... args) {
     return Ptr<T>(new T(args...));
   }
 
   template<typename T, typename... Args>
-  std::shared_ptr<T> create(Args... args) {
+  std::shared_ptr<T> create(Args const&... args) {
     return create_ptr<T, std::shared_ptr, Args...>(args...);
   }
 
